45e61f75b556cd55983be1a47a9fad32413574a8
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2022-11-03 11:21:52 +0300

    feat: updates on code base

---
+++ modules/it/src/main/java/com/tviplabs/api/playground/it/package-info.java
@@ -0,0 +1,7 @@
+/** Reactive Java API integration tests. */
+@InterfaceAudience.PublicApi
+@InterfaceStability.Evolving
+package com.tviplabs.api.playground.it;
+
+import com.tviplabs.api.playground.annotations.generic.InterfaceAudience;
+import com.tviplabs.api.playground.annotations.generic.InterfaceStability;
---
+++ modules/it/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * Reactive Java API integration tests module configuration file.
+ *
+ * @author Alexander Rogalskiy
+ */
+module reactive.java.api.samples {
+  requires reactive.java.api.interfaces;
+  requires reactive.java.api.annotations;
+  requires lombok;
+  requires org.slf4j;
+}
